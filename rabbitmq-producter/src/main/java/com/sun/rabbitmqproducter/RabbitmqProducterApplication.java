864c638e5ad9acf363ea7b29842e669942f6d33a
sunmiao <sunmiao@qq.com>
2019-09-18 11:00:05 +0800

    添加模块

---
+++ rabbitmq-producter/src/main/java/com/sun/rabbitmqproducter/RabbitmqProducterApplication.java
@@ -0,0 +1,13 @@
+package com.sun.rabbitmqproducter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProducterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProducterApplication.class, args);
+    }
+
+}
